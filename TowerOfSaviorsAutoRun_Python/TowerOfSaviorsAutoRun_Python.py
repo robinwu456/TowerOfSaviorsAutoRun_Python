25c3874c24a23b335804c66877dfd3c22faeea98
Robin <robinwu456@yahoo.com.tw>
2020-08-22 17:18:08 +0800

    中文字轉英文

--- TowerOfSaviorsAutoRun_Python/TowerOfSaviorsAutoRun_Python.py
+++ TowerOfSaviorsAutoRun_Python/TowerOfSaviorsAutoRun_Python.py
@@ -1,4 +1,5 @@
-encoding="utf-8"
+# !/usr/bin/python
+# coding:utf-8
 
 import sys  
 import os  
@@ -9,65 +10,53 @@ from com.dtmilano.android.viewclient import ViewClient
 def run():  
     stageRound = 6
 
-     # 連接手機
     device, serialno = ViewClient.connectToDeviceOrExit()  
     ##vc = ViewClient(device, serialno)         
 
     while True:
-        # 點擊"選擇屬性"
-        print("點擊 選擇屬性")
+        print("click btn choose attributes")
         device.touch(993, 439, 0)
         time.sleep(1)
 
-        # 選擇屬性(光)
-        print("選擇屬性(光)")
+        print("choose light")
         device.touch(474, 849, 0)
         time.sleep(1)
 
-        # 選擇魔族
-        print("選擇魔族")
+        print("choose demon")
         device.touch(225, 1007, 0)
         time.sleep(1)
 
-        # 按確定
-        print("按確定")
+        print("click btn confirm")
         device.touch(366, 1529, 0)
         time.sleep(1)
 
-        # 選擇隊友(index=1)
-        print("選擇隊友(index=1)")
+        print("choose ally (index=1)")
         device.touch(580, 700, 0)
         time.sleep(1)
 
-        # 點擊"選擇"
-        print("點擊 選擇")
+        print("click btn choose ally confirm")
         device.touch(544, 1218, 0)
         time.sleep(1)
 
-        # 點擊"進入戰鬥"
-        print("進入戰鬥...")
+        print("entering stage...")
         device.touch(945, 2223, 0)
         time.sleep(20)
 
-        # 轉珠
-        print("轉珠...")
+        print("turning beads ...")
         #device.touch(991, 1396, 0) # 右上第1顆
         #device.touch(1004, 1751, 0) # 右上第3顆
         for i in range(0, stageRound * 15):
-            print("轉珠次數" + str(i))
+            print("turn count: " + str(i))
             device._AdbClient__send("shell:input swipe 991 1396 991 1751", checkok=True, reconnect=True)
             time.sleep(0.5)
         
-        # 等待結算畫面
         time.sleep(30)
 
-        # 隨便點畫面
-        print("隨便點畫面")
+        print("exposure battle results")
         device.touch(564, 1043, 0)
         time.sleep(3)
 
-        # 點擊"再次挑戰"
-        print("點擊 再次挑戰")
+        print("battle again")
         device.touch(897, 1207, 0)
         time.sleep(5)
 
